3129c98ecd1cdb46a292b17a32f79aa44f2a05d8
3milychu <3milychu@gmail.com>
2018-05-04 18:02:03 -0400

    push

--- templates/sketch.js
+++ templates/sketch.js
@@ -1,12 +1,21 @@
+d3.json("https://raw.githubusercontent.com/3milychu/mm-exercise/master/static/data.json", function(json){
+    
+        Object.keys(json).forEach(function(key) {
+        var es = json['economic_stability'];
+    });
 
-d3.json("/api/gender-income-spending", function(json){
+    console.log(json);
 
-	json = json;
-	console.log(json);
+    var es = d3.nest()
+      .key(function(d) { return d.es; })
+      .rollup(function(v) { return v.length; })
+      .object(json);
+
+    console.log(es);
 
     d3.select("#viz ul")
         .selectAll("li")
-        .data(json.data)
+        .data(json)
         .enter()
         .append("li")
         .text(function(d) { return d.gender + " : " + d.income_max + " : " + d.income_avg + " : " + d.income_min; });
