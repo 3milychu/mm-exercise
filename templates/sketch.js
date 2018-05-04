b7f6ad444055aa46328f26c9c06cacc3e3da01d6
3milychu <3milychu@gmail.com>
2018-05-04 19:21:05 -0400

    push

--- templates/sketch.js
+++ templates/sketch.js
@@ -1,22 +1,26 @@
 d3.json("https://raw.githubusercontent.com/3milychu/mm-exercise/master/static/data.json", function(json){
-    
-        Object.keys(json).forEach(function(key) {
-        var es = json['economic_stability'];
+        
+    json.forEach(function(d) {
+        d.economic_stability = d.economic_stability;
     });
+
+    json = json.filter(function(d) { 
+        return (d.economic_stability != "economic_stability")});
 
     console.log(json);
 
     var es = d3.nest()
-      .key(function(d) { return d.es; })
-      .rollup(function(v) { return v.length; })
-      .object(json);
+        .key(function(d) { return d.economic_stability; })
+        .rollup(function(v) { return v.length; })
+        .entries(json)
+        .sort(function(a,b) {return d3.descending(a.values,b.values);});
 
     console.log(es);
 
     d3.select("#viz ul")
         .selectAll("li")
-        .data(json)
+        .data(es)
         .enter()
         .append("li")
-        .text(function(d) { return d.gender + " : " + d.income_max + " : " + d.income_avg + " : " + d.income_min; });
+        .text(function(d) { return "Economic Stability Level " + d.key + ": " + d.values});
 });
